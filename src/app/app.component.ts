71abbe263c48b8c167847e3251b63ce933a0d1c1
stevehndz <steve.r.hndz@gmail.com>
2024-03-10 21:04:35 -0600

    build: app and first component "saludo"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SaludoComponent } from "./saludo/saludo.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, SaludoComponent]
 })
 export class AppComponent {
   title = 'app';
---
+++ src/app/saludo/saludo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'saludo',
+  standalone: true,
+  imports: [],
+  templateUrl: './saludo.component.html',
+  styleUrl: './saludo.component.css'
+})
+export class SaludoComponent {
+  
+}
