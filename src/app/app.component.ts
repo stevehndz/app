112272c990d6be9dc7cda0d955b348bd7e21beb9
stevehndz <steve.r.hndz@gmail.com>
2024-03-11 12:13:03 -0600

    feat(evt): event component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SaludoComponent } from "./saludo/saludo.component";
 import { PadreComponent } from "./padre/padre.component";
+import { EventosComponent } from "./eventos/eventos.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, SaludoComponent, PadreComponent]
+    imports: [RouterOutlet, SaludoComponent, PadreComponent, EventosComponent]
 })
 export class AppComponent {
   title = 'app';
---
+++ src/app/eventos/eventos.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { log } from 'console';
+
+@Component({
+  selector: 'app-eventos',
+  standalone: true,
+  imports: [],
+  templateUrl: './eventos.component.html',
+  styleUrl: './eventos.component.css'
+})
+export class EventosComponent {
+  onClick() {
+    console.log('Se ha pulsado');
+
+  }
+}
