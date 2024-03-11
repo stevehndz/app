112a7881b8f29ee4ec1db1545c8593f0de37dff1
stevehndz <steve.r.hndz@gmail.com>
2024-03-11 12:07:52 -0600

    feat(bind): property binding

--- src/app/saludo/saludo.component.ts
+++ src/app/saludo/saludo.component.ts
@@ -9,13 +9,19 @@ import { Component } from '@angular/core';
 })
 export class SaludoComponent {
   mensaje: string; // Visualize this varaible only at this component
+  idParagraph: string; // To manage paragraph
+  idInput: string; // To manage input type
 
   constructor() {
     this.mensaje = 'Un saludo a todos';
+    this.idParagraph = 'Parrafo principal';
+    this.idInput = 'text';
 
     setTimeout(() => {
       // Time to change variable value
       this.mensaje = 'Otro mensaje diferente';
+      this.idParagraph = 'main';
+      this.idInput = 'password';
     }, 3000);
   }
 
